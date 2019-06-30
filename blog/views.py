fcd8d87cb6855acc08b6ac5299b10ece72a2a85d
birnamwood <birnamwood19@gmail.com>
2019-06-30 22:46:37 +0900

    Add Comment page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
+from .import forms
 
 
 def post_list(request):
@@ -10,6 +11,21 @@ def post_list(request):
 
 def article(request, pk):
   article = Post.objects.get(id=pk)
+  comments = Comment.objects.filter(post=article)
+
+  if request.method == "POST":
+    form = forms.commentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit=False)
+      comment.post = article
+      comment.author = request.user
+      comment.save()
+  else:
+    form = forms.CommentForm()
 
   print(article)
-  return render(request, 'blog/article.html',{ 'article': article })
+  return render(request, 'blog/article.html',{
+     'article': article,
+     'form': form,
+     'comments': comments
+     })
