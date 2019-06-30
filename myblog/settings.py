b43364cd50076ad98743c02539c32d19b26c5828
birnamwood <birnamwood19@gmail.com>
2019-06-30 18:56:32 +0900

    fix timezone

--- myblog/settings.py
+++ myblog/settings.py
@@ -104,9 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+#LANGUAGE_CODE = 'en-us'
+#TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
